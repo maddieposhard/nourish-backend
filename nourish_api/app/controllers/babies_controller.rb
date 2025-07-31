8433216f1cc39b9a3c1247590d1dc6d07da0be02
maddieposhard <poshardmadeline@gmail.com>
2025-07-31 14:48:55 -0500

    Added create function to baby controller, index to users, updated cors to connect to front end

--- nourish_api/app/controllers/babies_controller.rb
+++ nourish_api/app/controllers/babies_controller.rb
@@ -1,8 +1,21 @@
 class BabiesController < ApplicationController
+    def create
+        baby = Baby.new(baby_params)
+        if baby.save
+            render json: baby, status: :created
+        else
+            render json: { errors: baby.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        baby = Baby.find(params[:id])
+        render json: baby, status: :ok
+    end
 
     private
-    
+
     def baby_params
-        params.require(:baby).permit(:baby_id, :name)
+        params.require(:baby).permit(:user_id, :name)
     end
 end
--- nourish_api/app/controllers/users_controller.rb
+++ nourish_api/app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+    before_action :authenticate_request, except: [:index, :show]
+
+    def index 
+        users = User.all
+        render json: users, status: :ok
+    end
+
     def create
         user = User.new(user_params)
         if user.save
@@ -16,6 +23,6 @@ class UsersController < ApplicationController
     private
     
     def user_params
-      params.permit(:user_id, :email, :name, :password, :password_confirmation)
-    end
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end      
 end
