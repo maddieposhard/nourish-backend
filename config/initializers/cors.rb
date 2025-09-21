d184989b881665e4a45ada73b4f128233665f0b7
maddieposhard <poshardmadeline@gmail.com>
2025-09-21 17:34:50 -0500

    back to normal

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       resource '*',
         headers: :any,
         methods: [:get, :post, :patch, :put, :delete, :options, :head],
-        credentials: true
+        credentials: false
     end
   end
   
