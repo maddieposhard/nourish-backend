b08405778c09bfc2ee30b157dec7023e5dcae1ba
maddieposhard <poshardmadeline@gmail.com>
2025-09-21 17:30:04 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,10 @@
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :patch, :put, :delete, :options, :head],
-        credentials: false
-    end
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://nourish-frontend-wheat.vercel.app'  # be explicit in production
+    
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :patch, :put, :delete, :options, :head],
+      credentials: true
   end
-  
-  
-  
+end
